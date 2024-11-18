f5fd23e238be659245f500c902d63f9ffe0c3279
Ahmad Abdou <aa224rg@student.lnu.se>
2024-11-18 09:49:03 +0100

    fixing the tree

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,5 @@ const tree = new Tree('#taxonomy-element', 450, 450, margin);
 const barChart = new BarChart('.bar-chart', 450, 450);
 const axesPlot = new AxesPlot('#scatter-plot', 450, 450, margin);
 
-// Use them like:
 barChart.generateBars(idFilter1, idFilter2);
 axesPlot.showPlots(data);
