7c950c1a444c56ecb8ab903950f4118dd7bbc3ca
Ahmad Abdou <aa224rg@student.lnu.se>
2024-11-19 21:46:03 +0100

    Fixing the axis and adding the fox trajectory for the map

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,9 @@
 
-const margin = {left: 50, right: 50, top: 50, bottom: 65};
-const tree = new Tree('#taxonomy-element', 450, 450, margin);
-const barChart = new BarChart('.bar-chart', 450, 450);
-const axesPlot = new AxesPlot('#scatter-plot', 450, 450, margin);
+const margin2 = {left: 50, right: 50, top: 50, bottom: 65};
+// const tree2 = new Tree('#taxonomy-element', 450, 450, margin2);
+// const barChart2 = new BarChart('.bar-chart', 450, 450);
+const axesPlot = new AxesPlot('#scatter-plot', 500, 500, margin2);
 
-barChart.generateBars(idFilter1, idFilter2);
-axesPlot.showPlots(data);
+
+// barChart2.generateBars(idFilter1, idFilter2);
+// axesPlot2.showPlots(data);
