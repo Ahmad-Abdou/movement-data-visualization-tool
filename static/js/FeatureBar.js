22e567a97a8478f7178e4d85127178d2069bbed9
Ahmad Abdou <aa224rg@student.lnu.se>
2024-12-06 21:54:03 +0100

    Adding colors to the barchart

--- static/js/FeatureBar.js
+++ static/js/FeatureBar.js
@@ -16,14 +16,16 @@ class FeatureBar {
   }
 
    render (){
-
     const allRect = this.svg.selectAll('rect')
     .data(this.featureValue)
     .join('rect')
     .attr('width', (d)=> this.xScale(d))
     .attr('height', 10)
-    .attr('fill', '#0080FF')
-    .attr('y', (d, i)=> 15 * i +15)
+    .attr('fill', (d,i) => {
+      let feature = this.featurelabel[i]
+      return geometric.includes(feature) ? '#DC143C' : '#0080FF'
+    })
+    .attr('y', (d, i)=> 15 * i +27)
     .attr('x', 150)
     .attr('opacity', 0.5)
 
@@ -32,7 +34,7 @@ class FeatureBar {
     .join('text')
     .text(d=> d)
     .attr('fill', 'black')
-    .attr('y', (d, i)=> 15 * i +22)
+    .attr('y', (d, i)=> 15 * i +34)
     .attr('font-size', 10)
     .attr('x', 10)
 
@@ -41,9 +43,41 @@ class FeatureBar {
     .join('text')
     .text(d=> d.toString().substring(0,7))
     .attr('fill', 'black')
-    .attr('y', (d, i)=> 15 * i +22)
+    .attr('y', (d, i)=> 15 * i +34)
     .attr('font-size', 11)
     .attr('x', 160)
     .attr('font-weight', 700)
+
+    this.svg.append('rect')
+    .attr('width' , 56)
+    .attr('height' , 20)
+    .attr('fill' , '#0080FF')
+    .attr('opacity', 0.5)
+
+
+    this.svg.append('rect')
+    .attr('width' , 56)
+    .attr('height' , 20)
+    .attr('fill' , '#DC143C')
+    .attr('x' , 70)
+    .attr('opacity', 0.5)
+
+
+    this.svg.append('text')
+    .text('Kinematic')
+    .attr('font-size', 10)
+    .attr('fill', 'black')
+    .attr('x', 5)
+    .attr('y', 12)
+    .attr('font-weight', 700)
+
+    this.svg.append('text')
+    .text('Curvature')
+    .attr('font-size', 10)
+    .attr('fill', 'black')
+    .attr('x', 75)
+    .attr('y', 12)
+    .attr('font-weight', 700)
+
   }
 }
