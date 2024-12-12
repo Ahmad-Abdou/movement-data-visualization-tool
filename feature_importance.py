cf062313a710191f43fecdfc58a38751c9c9df43
Ahmad Abdou <aa224rg@student.lnu.se>
2024-12-12 16:34:47 +0100

    Fxiing the order for the taxonomies  and zone vs zone selection not displayed properly when selected from the tree

--- feature_importance.py
+++ feature_importance.py
@@ -65,3 +65,114 @@ def getData(zone, combination_path, features_path):
     except Exception as e:
         print(f"Error in getData: {str(e)}")
         raise
+
+def zone_logic_matcher(zone,x,y):
+    '''Returns TRUE if the zone matches the x and y values, otherwise returns FALSE!'''
+    matcher = {
+        0: (x < 0.5 and y < 0.5),
+        1: (x < 0.5 and y > 0.5 and x < (y - 0.5)),
+        2: (x > 0.5 and y < (x - 0.5)),
+        3: not((x < 0.5 and y < 0.5) or (x < 0.5 and y > 0.5 and x < (y - 0.5)) or (x > 0.5 and y < (x - 0.5)))
+    }
+    return matcher[zone]
+
+def logic_zone_matcher(x,y):
+    '''Returns the ZONE as an INTEGER for the current x and y values.'''
+    matcher = {
+        (x < 0.5 and y < 0.5): 0,
+        (x < 0.5 and y > 0.5 and x < (y - 0.5)): 1,
+        (x > 0.5 and y < (x - 0.5)): 2,
+        not((x < 0.5 and y < 0.5) or (x < 0.5 and y > 0.5 and x < (y - 0.5)) or (x > 0.5 and y < (x - 0.5))): 3
+    }
+    for logic, zone in matcher.items():
+        if logic == True:
+            return zone
+def getDataTwoZonesComparison(zoneA, zoneB, combination_path, features_path):
+    '''Sets zones as zoneA as 1, and zoneB as 0, only considers the 2 zones selected, drops DF entries that do not match either.
+        The RF model is then created only with df_features that were not dropped, labeled as 1 or 0.
+        '''
+    # NOTE: TEST PRINT CALL AT THE END WILL BE REMOVED UPON INTEGRATION WITH FRONT-END!
+    try:
+        # Read CSV files
+        df_combination = pd.read_csv(combination_path)
+        df_features = pd.read_csv(features_path)
+        
+        df_combination['ID'] = df_combination['ID'].astype(str)
+        df_features['ID'] = df_features['ID'].astype(str)
+        
+        df_features["zone"] = 0
+        
+        print("BEFORE FILTERING!\nDF FEATURES:", df_features)
+        print("DF SHAPE:", df_features.shape)
+        for _, row in df_combination.iterrows():
+            x, y = row['x'], row['y']
+            row_ID = str(row['ID'])
+            
+            # IF the current x,y is not a point in zoneA or zoneB
+            # We drop the row from the features, as it is not within the ONEvsONE in question.
+            if (logic_zone_matcher(x,y) != zoneA) and (logic_zone_matcher(x,y) != zoneB):
+                # print(row_ID)
+                df_features = df_features[df_features['ID'] != row_ID]
+            else:
+                if zone_logic_matcher(zoneA,x,y):
+                    df_features.loc[df_features['ID'] == row_ID, 'zone'] = 1
+                else:
+                    df_features.loc[df_features['ID'] == row_ID, 'zone'] = 0
+                # if zoneA == 0:
+                #     if zone_logic_matcher(zoneA,x,y):
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 1
+                #     else:
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 0
+                # elif zoneA == 1:
+                #     if zone_logic_matcher(zoneA,x,y):
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 1
+                #     else:
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 0
+                # elif zoneA == 2:
+                #     if zone_logic_matcher(zoneA,x,y):
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 1
+                #     else:
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 0
+                # elif zoneA == 3:
+                #     if zone_logic_matcher(zoneA,x,y):
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 1
+                #     else:
+                #         df_features.loc[df_features['ID'] == row_ID, 'zone'] = 0
+        y = df_features["zone"].astype(int)
+        X = df_features.drop(columns=["zone", "ID", "label"] if "label" in df_features.columns else ["zone", "ID"])
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+        rf_model = RandomForestClassifier(random_state=42)
+        rf_model.fit(X_train, y_train)
+        
+        y_pred_test = rf_model.predict(X_test)
+        
+        test_accuracy = accuracy_score(y_test, y_pred_test)
+        
+        if len(set(y_test)) == 1 and len(set(y_pred_test)) == 1:
+             if list(set(y_test))[0] == list(set(y_pred_test))[0]:
+                 f1 = 1.0
+             else:
+                 f1 = 0.0
+        else:
+            f1 = f1_score(y_test, y_pred_test, average='binary', zero_division=0)
+        # print(f"Zone {zone} statistics:")
+        # print(f"Number of samples in zone: {sum(y == 1)}")
+        # print(f"Number of samples outside zone: {sum(y == 0)}")
+        # print(f"Test set positive samples: {sum(y_test == 1)}")
+        # print(f"Test set predictions: {sum(y_pred_test == 1)}")
+        
+        feature_importance_df = pd.DataFrame({
+            'Feature': X.columns,
+            'Importance': rf_model.feature_importances_
+        }).sort_values(by='Importance', ascending=False)
+        # print(feature_importance_df)
+        # print(test_accuracy)
+        # print(f1)
+        print("AFTER FILTERING!\nDF FEATURES:", df_features)
+        print("DF SHAPE:", df_features.shape)
+        return feature_importance_df, test_accuracy, f1
+    except Exception as e:
+        print(f"Error in getData: {str(e)}")
+        raise
+# print(getDataTwoZonesComparison(2,3,"static/data_combination_foxes/Xspeed_Yacceleration_decision_scores.csv","df_with_ID/df_foxes_with_ID.csv"))
--- static/js/global.js
+++ static/js/global.js
@@ -8,34 +8,34 @@ let df_with_id = 'fox-df.csv';
 current_selectec_data = outlier_dataset_name
 
 let frequency_zone_combinations = {
-    "Kinematic Geometric": [],
-    "Speed Acceleration": [],
-    "Indentation Curvature": [],
+    "Geometric Kinematic": [],
+    "Acceleration Speed": [],
+    "Curvature Indentation": [],
     "Curvature Speed": [],
     "Indentation Speed": [],
-    "Curvature Acceleration": [],
-    "Indentation Acceleration": []  
+    "Acceleration Curvature": [],
+    "Acceleration Indentation": [] 
   };
 
 let file_mapping = {
-    "Kinematic Geometric": `../static/${outlier_dataset_name}/Xkinematic_Ygeometric_decision_scores.csv`,
-    "Speed Acceleration": `../static/${outlier_dataset_name}/Xspeed_Yacceleration_decision_scores.csv`,
-    "Indentation Curvature": `../static/${outlier_dataset_name}/Xindentation_Ycurvature_decision_scores.csv`,
+    "Geometric Kinematic": `../static/${outlier_dataset_name}/Xkinematic_Ygeometric_decision_scores.csv`,
+    "Acceleration Speed": `../static/${outlier_dataset_name}/Xspeed_Yacceleration_decision_scores.csv`,
+    "Curvature Indentation": `../static/${outlier_dataset_name}/Xindentation_Ycurvature_decision_scores.csv`,
     "Curvature Speed": `../static/${outlier_dataset_name}/Xcurvature_Yspeed_decision_scores.csv`,
     "Indentation Speed": `../static/${outlier_dataset_name}/Xindentation_Yspeed_decision_scores.csv`,
-    "Curvature Acceleration": `../static/${outlier_dataset_name}/Xcurvature_Yacceleration_decision_scores.csv`,
-    "Indentation Acceleration": `../static/${outlier_dataset_name}/Xindentation_Yacceleration_decision_scores.csv`        
+    "Acceleration Curvature": `../static/${outlier_dataset_name}/Xcurvature_Yacceleration_decision_scores.csv`,
+    "Acceleration Indentation": `../static/${outlier_dataset_name}/Xindentation_Yacceleration_decision_scores.csv`      
 };
 
 function setFileMapping(outlier_dataset_name){
     return {
-        "Kinematic Geometric": `../static/${outlier_dataset_name}/Xkinematic_Ygeometric_decision_scores.csv`,
-        "Speed Acceleration": `../static/${outlier_dataset_name}/Xspeed_Yacceleration_decision_scores.csv`,
-        "Indentation Curvature": `../static/${outlier_dataset_name}/Xindentation_Ycurvature_decision_scores.csv`,
+        "Geometric Kinematic": `../static/${outlier_dataset_name}/Xkinematic_Ygeometric_decision_scores.csv`,
+        "Acceleration Speed": `../static/${outlier_dataset_name}/Xspeed_Yacceleration_decision_scores.csv`,
+        "Curvature Indentation": `../static/${outlier_dataset_name}/Xindentation_Ycurvature_decision_scores.csv`,
         "Curvature Speed": `../static/${outlier_dataset_name}/Xcurvature_Yspeed_decision_scores.csv`,
         "Indentation Speed": `../static/${outlier_dataset_name}/Xindentation_Yspeed_decision_scores.csv`,
-        "Curvature Acceleration": `../static/${outlier_dataset_name}/Xcurvature_Yacceleration_decision_scores.csv`,
-        "Indentation Acceleration": `../static/${outlier_dataset_name}/Xindentation_Yacceleration_decision_scores.csv`        
+        "Acceleration Curvature": `../static/${outlier_dataset_name}/Xcurvature_Yacceleration_decision_scores.csv`,
+        "Acceleration Indentation": `../static/${outlier_dataset_name}/Xindentation_Yacceleration_decision_scores.csv`      
     }
 }
 file_mapping = setFileMapping(outlier_dataset_name)
@@ -64,7 +64,6 @@ async function selectFoxes(){
     trajectory_dataset_name = 'fox_trajectories.csv';
     df_with_id = 'fox-df.csv';
     file_mapping = setFileMapping(outlier_dataset_name)
-    // heatmap = new Heatmap('heat-map', 450, 450, margin_heat, frequency_zone_combinations);
     heatmap.render(file_mapping)
     current_selectec_data = outlier_dataset_name
 
@@ -74,7 +73,6 @@ async function selectHurricanes(){
     trajectory_dataset_name = 'hurricanes_trajectories.csv';
     df_with_id = 'hurricanes-df.csv';
     file_mapping = setFileMapping(outlier_dataset_name)
-    // heatmap = new Heatmap('heat-map', 450, 450, margin_heat, frequency_zone_combinations);
     heatmap.render(file_mapping)
     current_selectec_data = outlier_dataset_name
 
@@ -83,7 +81,6 @@ async function selectAIS(){
     outlier_dataset_name = 'data_combination_ais';
     trajectory_dataset_name = 'ais_trajectories.csv';
     file_mapping = setFileMapping(outlier_dataset_name)
-    // heatmap = new Heatmap('heat-map', 450, 450, margin_heat, frequency_zone_combinations);
     heatmap.render(file_mapping)
     current_selectec_data = outlier_dataset_name
 
@@ -122,7 +119,7 @@ const margin = {left: 50, right: 50, top:50, bottom:65};
 geometric = ["distance_geometry_1_1","distance_geometry_2_1","distance_geometry_2_2","distance_geometry_3_1","distance_geometry_3_2","distance_geometry_3_3","distance_geometry_4_1","distance_geometry_4_2","distance_geometry_4_3","distance_geometry_4_4","distance_geometry_5_1","distance_geometry_5_2","distance_geometry_5_3","distance_geometry_5_4","distance_geometry_5_5","angles_0s","angles_mean","angles_meanse","angles_quant_min","angles_quant_05","angles_quant_10","angles_quant_25","angles_quant_median","angles_quant_75","angles_quant_90","angles_quant_95","angles_quant_max","angles_range","angles_sd","angles_vcoef","angles_mad","angles_iqr","angles_skew","angles_kurt"]
 kinematic = ["speed_0s","speed_mean","speed_meanse","speed_quant_min","speed_quant_05","speed_quant_10","speed_quant_25","speed_quant_median","speed_quant_75","speed_quant_90","speed_quant_95","speed_quant_max","speed_range","speed_sd","speed_vcoef","speed_mad","speed_iqr","speed_skew","speed_kurt","acceleration_0s","acceleration_mean","acceleration_meanse","acceleration_quant_min","acceleration_quant_05","acceleration_quant_10","acceleration_quant_25","acceleration_quant_median","acceleration_quant_75","acceleration_quant_90","acceleration_quant_95","acceleration_quant_max","acceleration_range","acceleration_sd","acceleration_vcoef","acceleration_mad","acceleration_iqr","acceleration_skew","acceleration_kurt"]
 
-async function sendDataToPython(path_combination, zone, df_path_with_id) {
+async function sendDataToPython(path_combination, zoneA, zoneB ,df_path_with_id) {
     try {
         const cleanPath = path => path.replace('../static/', '');
         
@@ -133,7 +130,8 @@ async function sendDataToPython(path_combination, zone, df_path_with_id) {
             },
             body: JSON.stringify({
                 path_combination: cleanPath(path_combination),
-                zone: parseInt(zone),
+                zoneA: parseInt(zoneA),
+                zoneB: parseInt(zoneB),
                 df_path_with_id: cleanPath(df_path_with_id)
             })
         });
@@ -171,41 +169,43 @@ function displayFeatureImportance(data) {
 resultasdas = []
 function displayselectedZone() {
     let selector = document.getElementById("zone-select-1")
+    let selector2 = document.getElementById("zone-select-2")
+    let zoneA = null
     selector.addEventListener('change', async (e) => {
-        current_selected_zone = parseInt(e.target.value);
-        
-        if (current_selected_zone !== undefined && current_selected_combination) {
-            try {
-                const result = await sendDataToPython(
-                    file_mapping[current_selected_combination], 
-                    current_selected_zone, 
-                    file_mapping2[current_selectec_data]
-                );
+        current_selected_zone_1 = parseInt(e.target.value);
+        zoneA = current_selected_zone_1
+        if (current_selected_zone_1 !== undefined && current_selected_combination) {
+            // try {
+            //     const result = await sendDataToPython(
+            //         file_mapping[current_selected_combination], 
+            //         current_selected_zone_1, 
+            //         file_mapping2[current_selectec_data]
+            //     );
                 
-                if (result.status === 'success') {
-                    displayFeatureImportance(result.data);
-                    axesPlot.svg.selectAll('circle')
-                    .attr('fill', function(d) {
-                        const x = d.normalizedX;
-                        const y = d.normalizedY;
-                        const pointZone = getZoneForPoint(x, y);
-                        return pointZone === current_selected_zone ? '#ff0000' : 'grey';
-                    });
-                }
-            } catch (error) {
-                console.error('Failed to process data:', error);
-            }
+            //     if (result.status === 'success') {
+            //         displayFeatureImportance(result.data);
+            //         axesPlot.svg.selectAll('circle')
+            //         .attr('fill', function(d) {
+            //             const x = d.normalizedX;
+            //             const y = d.normalizedY;
+            //             const pointZone = getZoneForPoint(x, y);
+            //             return pointZone === current_selected_zone_1 ? '#ff0000' : 'grey';
+            //         });
+            //     }
+            // } catch (error) {
+            //     console.error('Failed to process data:', error);
+            // }
         }
     });
-    let selector2 = document.getElementById("zone-select-2")
     selector2.addEventListener('change', async (e) => {
-        current_selected_zone = parseInt(e.target.value);
+        zoneB = parseInt(e.target.value);
         
-        if (current_selected_zone !== undefined && current_selected_combination) {
+        if (zoneA !== undefined && zoneB !== undefined && current_selected_combination ) {
             try {
                 const result = await sendDataToPython(
                     file_mapping[current_selected_combination], 
-                    current_selected_zone, 
+                    zoneA,
+                    zoneB,
                     file_mapping2[current_selectec_data]
                 );
                 
@@ -216,7 +216,7 @@ function displayselectedZone() {
                         const x = d.normalizedX;
                         const y = d.normalizedY;
                         const pointZone = getZoneForPoint(x, y);
-                        return pointZone === current_selected_zone ? 'green' : 'grey';
+                        return pointZone === zoneB ? 'green' : 'grey';
                     });
                 }
             } catch (error) {
