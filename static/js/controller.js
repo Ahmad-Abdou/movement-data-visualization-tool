204e52b7ee2451122b2f4fae3b9eb1e19f84ebf5
Ahmad Abdou <aa224rg@student.lnu.se>
2025-02-16 18:59:53 +0100

    adding pinned button

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,5 +1,5 @@
 let isChecked = false
-
+let pinned_left = false
 d3.select("#checkbox_1").on("change", function() {
     isChecked = d3.select(this).property("checked");
 })
@@ -29,21 +29,109 @@ if (toggle_btn && trajectory && bar_chart) {
     });
 }
 
-
+const leftPanel = document.getElementById('left-container')
 const rightPanel = document.getElementById('right-container')
+const pin_right= document.getElementById('pin-right')
+const pin_left= document.getElementById('pin-left')
+
 rightPanel.addEventListener('mouseover', ()=> {
+    if(pin_right.className != 'pinned_right') {
         rightPanel.style.transform = 'translate(0px, 0)'
-        
+        pin_right.style.transform = 'translate(-475px, 0)'
+        pin_right.style.width = '60px'
+        pin_right.style.height = '100px'
+    }  
 })
 rightPanel.addEventListener('mouseout', ()=> {
-    rightPanel.style.transform = 'translate(99%, 0)'
+    if(pin_right.className != 'pinned_right') {
+        rightPanel.style.transform = 'translate(99%, 0)'
+        pin_right.style.transform = 'translate(0px, 0)'
+        pin_right.style.width = '20px'
+        pin_right.style.height = '160px'
+    }
+})
+pin_right.addEventListener('mouseover', ()=> {
+    if(pin_right.className != 'pinned_right') {
+        rightPanel.style.transform = 'translate(0px, 0)'
+        pin_right.style.transform = 'translate(-475px, 0)'
+        pin_right.style.width = '60px'
+        pin_right.style.height = '100px'
+    }
 })
 
-const leftPanel = document.getElementById('left-container')
+pin_right.addEventListener('mouseout', ()=> {
+    if(pin_right.className != 'pinned_right') {
+        rightPanel.style.transform = 'translate(99%, 0)'
+        pin_right.style.transform = 'translate(0, 0)'
+        pin_right.style.width = '20px'
+        pin_right.style.height = '160px'
+    }
+})
+
+pin_right.addEventListener('click',() => {
+    if(pin_right.className == 'pinned_right') {
+        pin_right.style.backgroundColor = '#0080FF'
+        pin_right.style.color = '#0080FF'
+        pin_right.className = ''
+        rightPanel.style.animationName = ''
+        rightPanel.style.animationDuration = '0s'
+    } else {
+        pin_right.className = 'pinned_right'
+        pin_right.style.backgroundColor = '#DC143C'
+        pin_right.style.color = '#DC143C'
+        rightPanel.style.animationName = ' horizontal-shaking'
+        rightPanel.style.animationDuration = '0.3s'
+    }
+})
+
 leftPanel.addEventListener('mouseover', ()=> {
+    if(pin_left.className != 'pinned_left') {
         leftPanel.style.transform = 'translate(0px, 0)'
-        
+        pin_left.style.transform = 'translate(475px, 0)'
+        pin_left.style.width = '60px'
+        pin_left.style.height = '100px'
+    }
 })
 leftPanel.addEventListener('mouseout', ()=> {
-    leftPanel.style.transform = 'translate(-99%, 0)'
+    if(pin_left.className != 'pinned_left') {
+        leftPanel.style.transform = 'translate(-99%, 0)'
+        pin_left.style.transform = 'translate(0px, 0)'
+        pin_left.style.width = '20px'
+        pin_left.style.height = '160px'
+    }
+
+})
+
+pin_left.addEventListener('mouseover', ()=> {
+    if(pin_left.className != 'pinned_left') {
+        leftPanel.style.transform = 'translate(0px, 0)'
+        pin_left.style.transform = 'translate(475px, 0)'
+        pin_left.style.width = '60px'
+        pin_left.style.height = '100px'
+    }
+
+})
+pin_left.addEventListener('mouseout', ()=> {
+    if(pin_left.className != 'pinned_left') {
+        leftPanel.style.transform = 'translate(-99%, 0)'
+        pin_left.style.transform = 'translate(0px, 0)'
+        pin_left.style.width = '20px'
+        pin_left.style.height = '160px'
+    }
+})
+
+pin_left.addEventListener('click',() => {
+    if(pin_left.className == 'pinned_left') {
+        pin_left.style.backgroundColor = '#0080FF'
+        pin_left.style.color = '#0080FF'
+        pin_left.className = ''
+        leftPanel.style.animationName = ''
+        leftPanel.style.animationDuration = '0s'
+    } else {
+        pin_left.className = 'pinned_left'
+        pin_left.style.backgroundColor = '#DC143C'
+        pin_left.style.color = '#DC143C'
+        leftPanel.style.animationName = ' horizontal-shaking'
+        leftPanel.style.animationDuration = '0.3s'
+    }
 })
