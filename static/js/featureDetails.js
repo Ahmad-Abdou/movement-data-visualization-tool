fe56d044a220c8e51eab18879fbd58ad34acf169
Ahmad Abdou <aa224rg@student.lnu.se>
2025-04-23 09:55:47 +0200

    Chnging the entier structure and design, removing time series as well

--- static/js/featureDetails.js
+++ static/js/featureDetails.js
@@ -49,20 +49,17 @@ class FeatureDetail {
     }
 
     const response = await fetch(`/api/feats/quantile?tid=${selectedTrajectory1},${selectedTrajectory2}&stats=${name}`);
-    
     if(!selectedTrajectory1) {
       notifyMessage('Please select a trajectory first')
     }
-    if (name.split("_")[1] == 'geometry') {
-      notifyMessage("Ask Amilcar!!!!")
-      return
-    }
+    // if (name.split("_")[1] == 'geometry') {
+    //   return
+    // }
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const responseData = await response.json();
-    console.log(responseData.results)
 
     if (!responseData || responseData.length === 0) {
         throw new Error('No data received');
@@ -87,145 +84,145 @@ class FeatureDetail {
 async showPercentile(y_lablel) {
   try {
 
-    this.svg.selectAll(".chart-content").remove();
+    // this.svg.selectAll(".chart-content").remove();
 
-    const headerGroup = this.svg.append('g').attr("class","chart-content").attr('id', 'header-group-feature-details')
+    // const headerGroup = this.svg.append('g').attr("class","chart-content").attr('id', 'header-group-feature-details')
 
-    headerGroup.append('rect')
-    .attr('width' , 70)
-    .attr('height' , 15)
-    .attr('fill' , kinematicColor)
-    .attr('x' , 240)
-    .attr('y' , -50)
+    // headerGroup.append('rect')
+    // .attr('width' , 70)
+    // .attr('height' , 15)
+    // .attr('fill' , kinematicColor)
+    // .attr('x' , 240)
+    // .attr('y' , -50)
 
-    headerGroup.append('rect')
-    .attr('width' , 70)
-    .attr('height' , 15)
-    .attr('fill' , geometricColor)
-    .attr('x' , 240)
-    .attr('y' , -30)
+    // headerGroup.append('rect')
+    // .attr('width' , 70)
+    // .attr('height' , 15)
+    // .attr('fill' , geometricColor)
+    // .attr('x' , 240)
+    // .attr('y' , -30)
 
-    headerGroup.append('text')
-    .text('Trajectory 1')
-    .attr('font-size', 10)
-    .attr('fill', 'black')
-    .attr('x' , 275)
-    .attr('y' , -40)
-    .attr('font-weight', 700)
-    .attr('text-anchor', 'middle')
-
-
-    headerGroup.append('text')
-    .text('Trajectory 2')
-    .attr('font-size', 10)
-    .attr('fill', 'black')
-    .attr('x' , 275)
-    .attr('y' , -20)
-    .attr('font-weight', 700)
-    .attr('text-anchor', 'middle')
-
-    headerGroup.append('text')
-    .text(this.operation.toString().slice(0,7))
-    .attr('x', 320)
-    .attr('y', -38)
-    .attr('font-size', 12)
-    .attr('font-weight', 700)
-    headerGroup.append('text')
-    .text(this.operation2.toString().slice(0,7))
-    .attr('x', 320)
-    .attr('y', -18)
-    .attr('font-size', 12)
-    .attr('font-weight', 700)
-
-    const chartGroup = this.svg.append("g")
-        .attr("class", "chart-content");
-
-    const yScale = d3.scaleLinear()
-        .domain([d3.min([...this.sub_trajectory, ...this.sub_trajectory2], d => d.feature), d3.max([...this.sub_trajectory, ...this.sub_trajectory2], d => d.feature)])
-        .range([this.height - 150, 0])
-
-    const xScaleIndexed = d3.scaleLinear()
-        .domain([0,10])
-        .range([0, this.width - 100]);
-
-    const line = d3.line()
-        .defined(d => !isNaN(d.feature))
-        .x((d,i) => xScaleIndexed(i))
-        .y(d => yScale(d.feature));
+    // headerGroup.append('text')
+    // .text('Trajectory 1')
+    // .attr('font-size', 10)
+    // .attr('fill', 'black')
+    // .attr('x' , 275)
+    // .attr('y' , -40)
+    // .attr('font-weight', 700)
+    // .attr('text-anchor', 'middle')
 
 
-    if(this.sub_trajectory.length != 0) {
-      chartGroup.append("path")
-      .datum(this.sub_trajectory)
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke","#0080FF")
-      .attr("stroke-width", 2)
-      .attr("d", line)
+    // headerGroup.append('text')
+    // .text('Trajectory 2')
+    // .attr('font-size', 10)
+    // .attr('fill', 'black')
+    // .attr('x' , 275)
+    // .attr('y' , -20)
+    // .attr('font-weight', 700)
+    // .attr('text-anchor', 'middle')
 
-      chartGroup.append("path")
-      .datum(this.sub_trajectory2)
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", "#DC143C80")
-      .attr("stroke-width", 2)
-      .attr("d", line)
+    // headerGroup.append('text')
+    // .text(this.operation.toString().slice(0,7))
+    // .attr('x', 320)
+    // .attr('y', -38)
+    // .attr('font-size', 12)
+    // .attr('font-weight', 700)
+    // headerGroup.append('text')
+    // .text(this.operation2.toString().slice(0,7))
+    // .attr('x', 320)
+    // .attr('y', -18)
+    // .attr('font-size', 12)
+    // .attr('font-weight', 700)
 
-      let splitted = y_lablel.split("_").splice(1).join("_"); 
-        if (splitted === 'quant_median' || splitted === 'mean'|| splitted === 'mad' || splitted === "meanse") {
-          chartGroup.append("line")
-          .attr("class", "stat-line")
-          .attr("x1", 0)                          
-          .attr("x2", this.width - 100)           
-          .attr("y1", yScale(this.operation))     
-          .attr("y2", yScale(this.operation))     
-          .attr("stroke", "#0080FF")
-          .attr("stroke-width", 2)
-          .style('stroke-dasharray', 5)
+    // const chartGroup = this.svg.append("g")
+    //     .attr("class", "chart-content");
 
-          chartGroup.append("line")
-          .attr("class", "stat-line")
-          .attr("x1", 0)                          
-          .attr("x2", this.width - 100)           
-          .attr("y1", yScale(this.operation2))     
-          .attr("y2", yScale(this.operation2))     
-          .attr("stroke", "#DC143C80")
-          .attr("stroke-width", 2)
-          .style('stroke-dasharray', 5)
+    // const yScale = d3.scaleLinear()
+    //     .domain([d3.min([...this.sub_trajectory, ...this.sub_trajectory2], d => d.feature), d3.max([...this.sub_trajectory, ...this.sub_trajectory2], d => d.feature)])
+    //     .range([this.height - 150, 0])
 
-        } else {
-          chartGroup.append('circle')
-          .attr('class', 'stat-circle')
-          .attr('r', 5)
-          .attr('cx', xScaleIndexed(5) )
-          .attr('cy', yScale(this.operation))
-          .attr('fill', "#0080FF")
+    // const xScaleIndexed = d3.scaleLinear()
+    //     .domain([0,10])
+    //     .range([0, this.width - 100]);
 
-          chartGroup.append('circle')
-          .attr('class', 'stat-circle')
-          .attr('r', 5)
-          .attr('cx', xScaleIndexed(5))
-          .attr('cy', yScale(this.operation2))
-          .attr('fill', "#DC143C80")
-        }
-        const xAxis = d3.axisBottom(xScaleIndexed)
-        .ticks(5)
+    // const line = d3.line()
+    //     .defined(d => !isNaN(d.feature))
+    //     .x((d,i) => xScaleIndexed(i))
+    //     .y(d => yScale(d.feature));
 
-    const yAxis = d3.axisLeft(yScale)
-        .ticks(5);
 
-    chartGroup.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", `translate(0, ${this.height - 150})`)
-        .call(xAxis);
+    // if(this.sub_trajectory.length != 0) {
+      // chartGroup.append("path")
+      // .datum(this.sub_trajectory)
+      // .attr("class", "line")
+      // .attr("fill", "none")
+      // .attr("stroke","#0080FF")
+      // .attr("stroke-width", 2)
+      // .attr("d", line)
 
-    chartGroup.append("g")
-        .attr("class", "y-axis")
-        .call(yAxis);
+      // chartGroup.append("path")
+      // .datum(this.sub_trajectory2)
+      // .attr("class", "line")
+      // .attr("fill", "none")
+      // .attr("stroke", "#DC143C80")
+      // .attr("stroke-width", 2)
+      // .attr("d", line)
+
+      // let splitted = y_lablel.split("_").splice(1).join("_"); 
+      //   if (splitted === 'quant_median' || splitted === 'mean'|| splitted === 'mad' || splitted === "meanse") {
+      //     chartGroup.append("line")
+      //     .attr("class", "stat-line")
+      //     .attr("x1", 0)                          
+      //     .attr("x2", this.width - 100)           
+      //     .attr("y1", yScale(this.operation))     
+      //     .attr("y2", yScale(this.operation))     
+      //     .attr("stroke", "#0080FF")
+      //     .attr("stroke-width", 2)
+      //     .style('stroke-dasharray', 5)
+
+      //     chartGroup.append("line")
+      //     .attr("class", "stat-line")
+      //     .attr("x1", 0)                          
+      //     .attr("x2", this.width - 100)           
+      //     .attr("y1", yScale(this.operation2))     
+      //     .attr("y2", yScale(this.operation2))     
+      //     .attr("stroke", "#DC143C80")
+      //     .attr("stroke-width", 2)
+      //     .style('stroke-dasharray', 5)
+
+      //   } else {
+      //     chartGroup.append('circle')
+      //     .attr('class', 'stat-circle')
+      //     .attr('r', 5)
+      //     .attr('cx', xScaleIndexed(5) )
+      //     .attr('cy', yScale(this.operation))
+      //     .attr('fill', "#0080FF")
+
+      //     chartGroup.append('circle')
+      //     .attr('class', 'stat-circle')
+      //     .attr('r', 5)
+      //     .attr('cx', xScaleIndexed(5))
+      //     .attr('cy', yScale(this.operation2))
+      //     .attr('fill', "#DC143C80")
+      //   }
+    //     const xAxis = d3.axisBottom(xScaleIndexed)
+    //     .ticks(5)
+
+    // const yAxis = d3.axisLeft(yScale)
+    //     .ticks(5);
+
+    // chartGroup.append("g")
+    //     .attr("class", "x-axis")
+    //     .attr("transform", `translate(0, ${this.height - 150})`)
+    //     .call(xAxis);
+
+    // chartGroup.append("g")
+    //     .attr("class", "y-axis")
+    //     .call(yAxis);
         const selectedFeature = y_lablel
           await mapGl.traject(this.data_without_filtering1, selectedTrajectory1, selectedFeature);
           await mapGl2.traject(this.data_without_filtering2, selectedTrajectory2, selectedFeature);
-    }    
+    // }    
   } catch (error) {
     console.error("Error drawing line chart:", error);
   }
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,13 @@
 const margin_heat = {top: 20, right: -150, bottom: -150, left: 120};
-let heatmap = new Heatmap('heat-map', 600, 325, margin_heat, frequency_zone_combinations);
+let heatmap = new Heatmap('heat-map', 600, 570, margin_heat, frequency_zone_combinations);
 heatmap.render(file_mapping);
 
 const margin_axes= {left: 50, right: 0, top: -35, bottom: 0};
-const axesPlot = new AxesPlot('#scatter-plot', 450, 350, margin_axes);
+const axesPlot = new AxesPlot('#scatter-plot', 450, 430, margin_axes);
 
-const tree = new Tree('#taxonomy-element', 500, 300, 0);
-
+const tree = new Tree('#taxonomy-element', 500, 500, 0);
+const featureBar = new FeatureBar('.feature-importance-container', 600, 500,null)
+featureBar.render()
 const mapGl = new MapGl('map')
 const mapGl2 = new MapGl('map-2')
 const heatmap_2d = d3.select('#heatmap-2d').attr('id', 'heatmap-2d').append('svg').attr("width", "100%").attr("height", "100%").style("background", "#f9f9f9");
